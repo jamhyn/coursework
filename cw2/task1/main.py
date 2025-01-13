168ab3c40a9d1e63ddad54b234e34941b0e9800f
jamhyn <jamhyn@proton.me>
2025-01-13 18:09:35 +0000

    commit

--- cw2/task1/main.py
+++ cw2/task1/main.py
@@ -1,1 +1,11 @@
-print('hello')
+def cyclicShift(message, shift):
+    if not message or shift < 0 or shift >= len(message):
+        return message
+
+    shift = shift % len(message)
+
+    return message[-shift:] + message[:-shift]
+
+def decryptCyclicShift(message, shift):
+    if not message or shift < 0 or shift >= len(message):
+        return message
